95a03706569066237c400908aa96e682fb629e03
MMEC69 <coorayeronnemanoshapineapple@gmail.com>
2024-02-10 17:19:31 +0530

    now items can be deleted or added

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -4,10 +4,13 @@ import AppReducer from './AppReducer'
 //intailize
 const initialState = {
     transactions: [
+        //test data
+        /*
         { id: 1, text: 'Flower', amount: -20 },
         { id: 3, text: 'Book', amount: -10 },
         { id: 2, text: 'Salary', amount: 300 },
         { id: 4, text: 'Camera', amount: 150 }
+        */
     ]
 }
 
@@ -19,9 +22,23 @@ export const GlobalProvider = ({children}) =>{
     //reducer
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    const deleteTransaction = (id) =>{
+        dispatch({
+            type: "DELETE TRANSACTION",
+            payload: id
+        });
+    }
+
+    const addTransaction = (transaction) =>{
+        dispatch({
+            type: "ADD TRANSACTION",
+            payload: transaction
+        });
+    }
+
     //return
     return (
-        <GlobalContext.Provider value = {{transactions: state.transactions}}>
+        <GlobalContext.Provider value = {{transactions: state.transactions, deleteTransaction, addTransaction}}>
             {children}
         </GlobalContext.Provider>
     );
